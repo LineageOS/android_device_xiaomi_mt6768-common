c5f9626f7f1375867de78a80dd93f544410676eb
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-03 00:47:16 +0800

    mt6768-common: Include a default dalvik heap config
    * We set dalvik heap by libinit in the ROM, however libinit does not
      exist on GSIs.
    
    Change-Id: I21bc505041a2e50802f742a118ff0f31b5e65b87

--- mt6768.mk
+++ mt6768.mk
@@ -7,6 +7,9 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Default dalvik heap config
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
