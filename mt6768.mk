f85ee38cb74e2a2d15661346e2e35bc1d3a5c2c7
Ben Grisdale <bengris32@protonmail.ch>
2026-07-12 20:46:11 +0200

    mt6768-common: Include chipinfo script
    Change-Id: Iac5b0bfb499bc6cc1d075e43224dfdc9a6312b24
    Signed-off-by: Ben Grisdale <bengris32@protonmail.ch>

--- mt6768.mk
+++ mt6768.mk
@@ -58,6 +58,10 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Chipinfo
+PRODUCT_PACKAGES += \
+    chipinfo
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
