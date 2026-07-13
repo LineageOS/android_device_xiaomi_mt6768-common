311aa16afa3da3591cf6f248627060ba0a0398b8
Matsvei Niaverau <matvej2002@gmail.com>
2026-07-13 14:02:05 +0200

    mt6768-common: Build IR AIDL service
    Change-Id: Id49aeb69eb11e2c5c6a62b5bb9f8cd00fd8380a4

--- mt6768.mk
+++ mt6768.mk
@@ -121,8 +121,7 @@ PRODUCT_COPY_FILES += \
 
 # IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir-service.example
 
 # Lights
 PRODUCT_PACKAGES += \
