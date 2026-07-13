a1ec6d9d1101db11b695e050095f0dbfd054a788
Giovanni Ricca <giovanniricca@proton.me>
2026-07-13 13:21:12 +0200

    mt6768-common: Move mediatek ims boot jar declaration to vendor
    Change-Id: I8eb961ac31326eaadd2bb3d10d2ecbe2e95c233b

--- mt6768.mk
+++ mt6768.mk
@@ -110,15 +110,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-service
 
 # IMS
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
 
