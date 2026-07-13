7c602530e9ede6fa93567a9cdc357d086592c3c5
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-13 13:22:48 +0200

    mt6768-common: Build mediatek-common JAR from source
    Reference: I7967d31f0d1799610c6d31d4d6e48f0ccc77a7f2
    Change-Id: I671cb1758288f6ca565495366c6a835a6e7a0ec5

--- mt6768.mk
+++ mt6768.mk
@@ -111,6 +111,7 @@ PRODUCT_PACKAGES += \
 
 # IMS
 $(call inherit-product, hardware/lineage/compat/frameworks/compat.mk)
+$(call inherit-product, hardware/mediatek/frameworks/mediatek-frameworks.mk)
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
