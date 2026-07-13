9f8481cfb421c5231e11faf8ac29a3025337cd55
Giovanni Ricca <giovanniricca@proton.me>
2026-07-13 13:36:34 +0200

    mt6768-common: Inherit common Mediatek FCM
    Change-Id: I2807b7f2b0c86305fea56f28f0137ea2166ebade

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -165,7 +165,9 @@ TARGET_VIBRATOR_SUPPORTS_EFFECTS := true
 # VINTF
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+	$(COMMON_PATH)/framework_compatibility_matrix.xml \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
 
 # Wi-Fi
 WPA_SUPPLICANT_VERSION := VER_0_8_X
