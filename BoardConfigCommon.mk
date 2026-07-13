0df966dda9ff407e72a560517a6435667c7b6aff
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-07-13 13:38:17 +0200

    mt6768-common: Use hardware/xiaomi FCM
    Change-Id: Ib1dcdfe20ef5ffaf1b3b407b47790ee029a2755f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -166,8 +166,8 @@ TARGET_VIBRATOR_SUPPORTS_EFFECTS := true
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-	$(COMMON_PATH)/framework_compatibility_matrix.xml \
-    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
+	hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
 # Wi-Fi
 WPA_SUPPLICANT_VERSION := VER_0_8_X
