0d5d02f817c86f7bd9ceca091b9060526947e577
bengris32 <bengris32@protonmail.ch>
2026-07-16 15:46:40 +0200

    mt6768-common: Switch to Pixel thermal service
    * Config based off "dumpsys thermalservice" using stock thermal service.
    
    Co-authored-by: Woomymy <woomy@woomy.be>
    Change-Id: I92fcc16ce01aae8b1b9307c38f4236dffb7f78b9
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- mt6768.mk
+++ mt6768.mk
@@ -241,7 +241,10 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl
+    android.hardware.thermal-service.mediatek
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # UFFD GC
 PRODUCT_ENABLE_UFFD_GC := true
