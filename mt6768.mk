f5495731e9f7799b49bec23cffe6f7ed57b3a25f
Matsvei Niaverau <matvej2002@gmail.com>
2026-07-18 18:26:24 +0200

    mt6768-common: Decommonize thermal config
    Co-authored-by: R0rt1z2 <roger@r0rt1z2.com>
    Change-Id: I8984471f6d39473cbff906e094324cfb5d82e9ba

--- mt6768.mk
+++ mt6768.mk
@@ -243,9 +243,6 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.mediatek
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
-
 # UFFD GC
 PRODUCT_ENABLE_UFFD_GC := true
 
