59ac2fd1d3b939472a67c5325d4cd674ad249365
Bowgo Tsai <bowgotsai@google.com>
2026-07-30 20:31:08 +0200

    mt6768-common: Removing GSI keys
    Those keys will be embedded into VtsSecurityAvb on host side
    instead, to verify the GSI image used on the device.
    
    Bug: 149806769
    Test: build and checks those keys are removed from
          $OUT/recovery/root/first_stage_ramdisk/avb/
    Change-Id: I8a002ba6f1421fb460056ccae6572050bdb0ce3c

--- mt6768.mk
+++ mt6768.mk
@@ -7,9 +7,6 @@
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
-
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
